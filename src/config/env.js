b444161a818086a483f5c88d04748102bf48dbcb
Kayode <bubykay@gmail.com>
2025-03-30 19:59:58 +0100

    config

--- src/config/env.js
+++ src/config/env.js
@@ -1,21 +1,21 @@
 import dotenv from "dotenv";
 import { getEnvVariable } from "../utils/index.js";
 import rateLimit from "express-rate-limit";
-dotenv.config()
-
+dotenv.config();
 
 export default {
-    port: getEnvVariable("port"),
-    swaggerDomain: getEnvVariable('swagger_domain'),
-    nodeEnv: getEnvVariable('node_env'),
-    isProduction: getEnvVariable('is_production'),
-    rateLimit: ()=> rateLimit({
-            windowMs: Number(getEnvVariable("rate_limit_time")) * 60 * 1000, // 15 minutes
-            max: Number(getEnvVariable("rate_limit_max")), // 100 requests per IP
-            message: {message:"Too many requests, please try again later."},
-            standardHeaders: false, // Includes RateLimit headers
-            legacyHeaders: true, // Disable deprecated headers
-        })
-    
-    
+  port: getEnvVariable("port"),
+  swaggerDomain: getEnvVariable("swagger_domain"),
+  nodeEnv: getEnvVariable("node_env"),
+  isProduction: getEnvVariable("is_production"),
+  rateLimit: () =>
+    rateLimit({
+      windowMs: Number(getEnvVariable("rate_limit_time")) * 60 * 1000, // 15 minutes
+      max: Number(getEnvVariable("rate_limit_max")), // 100 requests per IP
+      message: { message: "Too many requests, please try again later." },
+      standardHeaders: false, // Includes RateLimit headers
+      legacyHeaders: true, // Disable deprecated headers
+    }),
+
+  baseUrl: getEnvVariable("base_url"),
 };
